46dc519807da753babbcf699217a8ccce59cba8f
Luke Valentine <lukevalentine@csm-wl-dhcp-199-64.mines.edu>
2020-02-25 13:49:46 -0700

    Board updated

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -1,5 +1,35 @@
 package clueGame;
 
-public class Board {
+import java.util.Map;
+import java.util.Set;
 
+public class Board {
+	private int numRows;
+	private int numColumns;
+	public final int MAX_BOARD_SIZE = 25;
+	private BoardCell[][] board;
+	private Map<Character,String> legend;
+	private Map<BoardCell,Set<BoardCell>> adjMatrix;
+	private Set<BoardCell> targets;
+	private String boardConfigFile;
+	private String roomConfigFile;
+	
+	public Board getInstance() {
+		return null;
+	}
+	public void initialize() {
+		
+	}
+	public void loadRoomConfig() {
+		
+	}
+	public void loadBoardConfig() {
+		
+	}
+	public void calcAdjacencies() {
+		
+	}
+	public void calcTargets(BoardCell cell, int pathLength) {
+		
+	}
 }
