b8f40e41a75bca21e8b1b9c76b41cc3753fb9833
Luke Valentine <lukevalentine@Lukes-MacBook-Pro-3.local>
2020-03-12 14:29:09 -0600

    Corrected all indentation (didn't save lastime hence the repetitive commit)

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -28,28 +28,31 @@ public class Board {
 
 	// variable used for singleton pattern
 	private static Board theInstance = new Board();
-	//constructor is private to ensure only one copy
-	private Board() {}
+
+	// constructor is private to ensure only one copy
+	private Board() {
+	}
+
 	// this method returns the only Board
 	public static Board getInstance() {
 		return theInstance;
 	}
 
 	public void initialize() {
-			try {
-				loadRoomConfig(roomConfigFile);
-			} catch (IOException e) {
-				System.out.println("Trouble loading roomConfigFile - txt");
-			}
-			try {
-				loadBoardConfig(boardConfigFile);
-			} catch (IOException e) {
-				System.out.println("Trouble loading boardConfigFile - csv");
-			}
-		
+		try {
+			loadRoomConfig(roomConfigFile);
+		} catch (IOException e) {
+			System.out.println("Trouble loading roomConfigFile - txt");
+		}
+		try {
+			loadBoardConfig(boardConfigFile);
+		} catch (IOException e) {
+			System.out.println("Trouble loading boardConfigFile - csv");
+		}
+
 		calcAdjacencies();
 	}
-	
+
 	public void calcAdjacencies() {
 		for (int row = 0; row < numRows; row++) {
 			for (int col = 0; col < numColumns; col++) {
@@ -57,7 +60,7 @@ public class Board {
 				BoardCell testCell = new BoardCell();
 				BoardCell startCell = new BoardCell();
 				startCell = board[row][col];
-				//up
+				// up
 				if (row > 0) {
 					testCell = board[row - 1][col];
 					if (adjHelp(testCell, startCell))
@@ -73,7 +76,7 @@ public class Board {
 				if (col < numColumns - 1) {
 					testCell = board[row][col + 1];
 					if (adjHelp(testCell, startCell))
-						adjSet.add(board[row][col + 1] );
+						adjSet.add(board[row][col + 1]);
 				}
 				// left
 				if (col > 0) {
@@ -84,9 +87,9 @@ public class Board {
 				// push the adj list and corresponding cell to map
 				adjMatrix.put(getCellAt(row, col), adjSet);
 			}
-		}	
+		}
 	}
-	
+
 	public HashSet<BoardCell> calcTargets(int row, int col, int dieRoll) {
 		// create new hashset
 		BoardCell startCell = getCellAt(row, col);
@@ -98,9 +101,9 @@ public class Board {
 		foundTargets = findAllTargets(startCell, dieRoll, originalStart);
 		// remove the startCell if it exists (illegal move)
 		foundTargets.remove(startCell);
-		return (foundTargets);	
+		return (foundTargets);
 	}
-	
+
 	public HashSet<BoardCell> findAllTargets(BoardCell startCell, int stepsRemaining, BoardCell startingSquare) {
 		// go through every adj cell
 		for (BoardCell testCell : getAdjList(startCell.getRow(), startCell.getColumn())) {
@@ -108,7 +111,7 @@ public class Board {
 				visited.add(startingSquare);
 				continue;
 			}
-			if(testCell.isDoorway() && isGoodDoor(testCell,startCell)) {
+			if (testCell.isDoorway() && isGoodDoor(testCell, startCell)) {
 				targets.add(testCell);
 				continue;
 			}
@@ -119,11 +122,11 @@ public class Board {
 			visited.add(testCell);
 			// if numsteps are 1 we have reached are target
 			if (stepsRemaining == 1) {
-				//System.out.println(b.getInitialFull());
+				// System.out.println(b.getInitialFull());
 				targets.add(testCell);
 			}
 			// else call recursive
-			else if(!testCell.isDoorway()){
+			else if (!testCell.isDoorway()) {
 				findAllTargets(testCell, stepsRemaining - 1, startingSquare);
 			}
 			visited.remove(testCell);
@@ -133,94 +136,89 @@ public class Board {
 	}
 
 	/*
-	 * *************************HELPER FUNCTIONS*************************************
+	 * *************************HELPER
+	 * FUNCTIONS*************************************
 	 */
-	
+
 	public boolean adjHelp(BoardCell testCell, BoardCell startCell) {
-		//if the cell is not a doorway and it has two initials don't enter
-		//if it is not an exit and don't leave the room
-		//if you are on a walkway only enter if door
-		//if it is a walkway you can keep walking on the walkway
-		if (
-				(!(testCell.isDoorway() && isGoodDoor(testCell,startCell)) && testCell.getInitials().length() == 2) 
-				|| 
-				((!isExitSquare(testCell, startCell)) && startCell.getInitials().length() == 2)
-				|| 
-				((isSameRoom(testCell,startCell) && startCell.getFirstInitial() != 'W'))
-				||
-				(startCell.getFirstInitial() == 'W' && testCell.getFirstInitial() != 'W' && testCell.getInitials().length() !=2)
-				||
-				(startCell.getFirstInitial() != 'W' && testCell.getFirstInitial() == 'W' && testCell.getInitials().length() !=2 && startCell.getInitials().length() != 2)) {
+		// if the cell is not a doorway and it has two initials don't enter
+		// if it is not an exit and don't leave the room
+		// if you are on a walkway only enter if door
+		// if it is a walkway you can keep walking on the walkway
+		if ((!(testCell.isDoorway() && isGoodDoor(testCell, startCell)) && testCell.getInitials().length() == 2)
+				|| ((!isExitSquare(testCell, startCell)) && startCell.getInitials().length() == 2)
+				|| ((isSameRoom(testCell, startCell) && startCell.getFirstInitial() != 'W'))
+				|| (startCell.getFirstInitial() == 'W' && testCell.getFirstInitial() != 'W'
+				&& testCell.getInitials().length() != 2)
+				|| (startCell.getFirstInitial() != 'W' && testCell.getFirstInitial() == 'W'
+				&& testCell.getInitials().length() != 2 && startCell.getInitials().length() != 2)) {
 			return false;
 		}
-		//otherwise you can not move
+		// otherwise you can not move
 		else {
 			return true;
 		}
 	}
 
-	//checks if target is the right placement to exit based on starting origin being a door and it's direction
+	// checks if target is the right placement to exit based on starting origin
+	// being a door and it's direction
 	public boolean isExitSquare(BoardCell target, BoardCell origin) {
-		//a walkway above a door with direction up
-		if(origin.getRow() > target.getRow() && origin.getDoorDirection() == DoorDirection.UP) {
+		// a walkway above a door with direction up
+		if (origin.getRow() > target.getRow() && origin.getDoorDirection() == DoorDirection.UP) {
 			return true;
 		}
-		//a walkway below a door with direction down
-		else if(origin.getRow() < target.getRow() && origin.getDoorDirection() == DoorDirection.DOWN) {
+		// a walkway below a door with direction down
+		else if (origin.getRow() < target.getRow() && origin.getDoorDirection() == DoorDirection.DOWN) {
 			return true;
 		}
-		//a walkway to the left of a door with direction left
-		else if(origin.getColumn() > target.getColumn() && origin.getDoorDirection() == DoorDirection.LEFT) {
+		// a walkway to the left of a door with direction left
+		else if (origin.getColumn() > target.getColumn() && origin.getDoorDirection() == DoorDirection.LEFT) {
 			return true;
 		}
-		//a walkway to the right of a door with direction right
-		else if(origin.getColumn() < target.getColumn() && origin.getDoorDirection() == DoorDirection.RIGHT) {
+		// a walkway to the right of a door with direction right
+		else if (origin.getColumn() < target.getColumn() && origin.getDoorDirection() == DoorDirection.RIGHT) {
 			return true;
-		}
-		else {
+		} else {
 			return false;
 		}
 	}
-	
-	//checks door direction and adds to targets if it works
+
+	// checks door direction and adds to targets if it works
 	public boolean isGoodDoor(BoardCell target, BoardCell origin) {
 		// a cell under a door, and door direction is up
-		if(origin.getRow() < target.getRow() && target.getDoorDirection() == DoorDirection.UP) {
-				return true;
-		}
-		//walkway is above door and door direction is down
-		else if(origin.getRow() > target.getRow() && target.getDoorDirection() == DoorDirection.DOWN) {
-				return true;
-		}
-		//walkway is to the left of door and door direction is left
-		else if(origin.getColumn() < target.getColumn() && target.getDoorDirection() == DoorDirection.LEFT) {
-				return true;
-		}
-		//walkway is to the right of door and door direction is right
-		else if(origin.getColumn() > target.getColumn() && target.getDoorDirection() == DoorDirection.RIGHT) {
-				return true;
-		}
-		else {
-			return false;
-		}
-	}
-	
-	//checks to see if the target haas the same initial as the origin cell
-	public boolean isSameRoom(BoardCell target, BoardCell origin){
-		if(origin.getFirstInitial() == target.getFirstInitial()) {
+		if (origin.getRow() < target.getRow() && target.getDoorDirection() == DoorDirection.UP) {
 			return true;
 		}
-		else {
+		// walkway is above door and door direction is down
+		else if (origin.getRow() > target.getRow() && target.getDoorDirection() == DoorDirection.DOWN) {
+			return true;
+		}
+		// walkway is to the left of door and door direction is left
+		else if (origin.getColumn() < target.getColumn() && target.getDoorDirection() == DoorDirection.LEFT) {
+			return true;
+		}
+		// walkway is to the right of door and door direction is right
+		else if (origin.getColumn() > target.getColumn() && target.getDoorDirection() == DoorDirection.RIGHT) {
+			return true;
+		} else {
 			return false;
 		}
 	}
-	
-	//checks if the symbol is a closet
+
+	// checks to see if the target haas the same initial as the origin cell
+	public boolean isSameRoom(BoardCell target, BoardCell origin) {
+		if (origin.getFirstInitial() == target.getFirstInitial()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	// checks if the symbol is a closet
 	public boolean isCloset(BoardCell target) {
-		if(target.getFirstInitial() == 'X') {
+		if (target.getFirstInitial() == 'X') {
 			return true;
-		}
-		else { 
+		} else {
 			return false;
 		}
 	}
@@ -229,25 +227,25 @@ public class Board {
 	 * ********************CONFIGURATION METHODS********************************
 	 */
 
-	public void setConfigFiles(String csv, String txt){
+	public void setConfigFiles(String csv, String txt) {
 		boardConfigFile = csv;
 		roomConfigFile = txt;
 	}
-	
+
 	public void loadRoomConfig(String legend_txt) throws IOException {
 		String line = "";
-        String splitBy = ", ";
-        String[] lines;
-        legend = new HashMap<Character, String>();
-        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(legend_txt), "UTF-8"));
-        while ((line = br.readLine()) != null) {
-            lines = line.split(splitBy);
-            char x = lines[0].charAt(0);
-            legend.put(x, lines[1]);
-            }
-        br.close();
-        }	
-	
+		String splitBy = ", ";
+		String[] lines;
+		legend = new HashMap<Character, String>();
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(legend_txt), "UTF-8"));
+		while ((line = br.readLine()) != null) {
+			lines = line.split(splitBy);
+			char x = lines[0].charAt(0);
+			legend.put(x, lines[1]);
+		}
+		br.close();
+	}
+
 	public void loadBoardConfig(String layout_csv) throws IOException {
 		// set up file reader for csv
 		BufferedReader in = new BufferedReader(new FileReader(layout_csv));
@@ -257,14 +255,14 @@ public class Board {
 		// used to make sure there are the same number of columns in each row
 		int checked = 0;
 		int cols_seen = 0;
-		while((line = in.readLine()) != null) {
+		while ((line = in.readLine()) != null) {
 			String[] initials = line.split(",");
-			for(String i: initials) {
+			for (String i : initials) {
 				board[row][col] = new BoardCell(row, col, i);
 				col++;
 			}
-			if(col != cols_seen) {
-				if(checked == 0) {
+			if (col != cols_seen) {
+				if (checked == 0) {
 					cols_seen = col;
 					checked += 1;
 				} else {
@@ -278,20 +276,20 @@ public class Board {
 		numRows = row;
 		numColumns = cols_seen;
 	}
-	
+
 	/*
 	 ************************** GETTERS AND SETTERS *********************************
 	 */
-	
+
 	public Set<BoardCell> getAdjList(int row, int col) {
-		return adjMatrix.get(getCellAt(row,col));
+		return adjMatrix.get(getCellAt(row, col));
 	}
 
 	public BoardCell getCellAt(int row, int col) {
 		// return the corresponding2D array
 		return board[row][col];
 	}
-		
+
 	public Set<BoardCell> getTargets() {
 		return targets;
 	}
@@ -307,6 +305,5 @@ public class Board {
 	public Map<Character, String> getLegend() {
 		return legend;
 	}
-
 
 }
