cd79007a615fc9ba1562104a30dcbb54db45119f
BryannaGaede <bpriddy@mymail.mines.edu>
2020-03-12 18:53:00 -0600

    PASSING TEST: check if human or computer

--- src/clueGame/ComputerPlayer.java
+++ src/clueGame/ComputerPlayer.java
@@ -5,6 +5,7 @@ import java.util.Set;
 public class ComputerPlayer extends Player {
 	public ComputerPlayer(String name, int row2, int col, String color) {
 		super(name, row2, col, color);
+		super.status = Status.COMPUTER;
 	}
 	public BoardCell pickLocaation(Set<BoardCell> targets) {
 		return null;
--- src/clueGame/HumanPlayer.java
+++ src/clueGame/HumanPlayer.java
@@ -3,5 +3,6 @@ package clueGame;
 public class HumanPlayer extends Player {
 	public HumanPlayer(String name, int row2, int col, String color) {
 		super(name, row2, col, color);
+		super.status = Status.HUMAN;
 	}
 }
